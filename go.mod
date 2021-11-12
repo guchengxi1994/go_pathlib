2444083d961a65b5ff65514d20a17683947524bf
guchengxi1994 <guchengxi1994@qq.com>
2021-11-12 16:28:13 +0800

    fix name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module goPathlib
+module github.com/guchengxi1994/go_pathlib
 
 go 1.16
--- go_test.go
+++ go_test.go
@@ -2,7 +2,6 @@ package gopathlib_test
 
 import (
 	"fmt"
-	goPathlib "goPathlib/src"
 	"path"
 	"testing"
 )
@@ -15,10 +14,4 @@ func Test_split_ext(t *testing.T) {
 	s2 := "c:\\docs\\a.tx\\t"
 	ext = path.Ext(s2)
 	fmt.Println(ext)
-
-	m := goPathlib.SplitExt(s)
-	fmt.Printf("m: %v\n", m)
-	m = goPathlib.SplitExt(s2)
-	fmt.Printf("m: %v\n", m)
-
 }
